f24e5e346b32388c5406b40c5f14e4ddb1a2babf
Robert Bittick <robertbittick@gmail.com>
2022-10-29 21:53:32 -0700

    commit tree 4ee40f6e9461808ea24d1db99fcd8f1e82579fdb

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  return array.some(function (currentValue, index, arrayRef) {
+     const operationArray = [...arrayRef];
+     operationArray.splice(index, 1);
+     return operationArray.includes(target - currentValue);
+  });
+
 }
 
 /* 
@@ -8,10 +14,39 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  hasTargetSum(array-of-numbers, target number) {
+    *add element 0 of ARRAY-OF-NUMBERS to the next element in the array*
+
+    const COMPARED-SUM = ARRAY-OF-NUMBERS[firstElement] + ARRAY-OF-NUMBERS[nextElement]
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j< array.length; j++) {
+        if (TARGET NUMBER === COMPARED-SUM  ) {
+        return true
+      }
+    }
+    
+    }
+  }
+
 */
 
+// function hasTargetSum(array, target) {
+//   const newSum = []
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (target === array[i] + array[j]) newSum.push([array[i],array[j]])
+//         return true
+//     }
+//   }
+// }
+
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
+
+  the function takes an array of numbers and a target number.
+    the function searches the array for two numbers that add together to the target number
+      it will take one number and add it to each number in the array until it finds a match === to the target number
+        it will then return true or false
 */
 
 // You can run `node index.js` to view these console logs
